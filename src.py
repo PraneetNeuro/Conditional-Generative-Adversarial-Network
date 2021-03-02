263292aae47b493dc384d99c9f4304a7e762d4eb
Praneet <praneetquantumx@gmail.com>
2021-03-02 17:12:08 +0530

    Update src.py

--- src.py
+++ src.py
@@ -165,7 +165,7 @@ class GAN:
             fake_output)
 #         real_loss = self.cross_entropy(tf.ones_like(real_output), real_output)
 #         fake_loss = self.cross_entropy(tf.zeros_like(fake_output), fake_output)
-#         total_loss = real_loss + fake_loss
+        total_loss = real_loss + fake_loss
         return total_loss
 
     def generator_loss(self, fake_output, real_y):
